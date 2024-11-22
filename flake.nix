14a360956a1ab68aeda991b1b1a785d9f840fd98
Jose San Leandro Armendáriz <github@acm-sl.org>
2024-11-22 20:10:28 +0100

    Commit created with sync-pythoneda.sh

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
         "pythoneda-shared-pythonlang-banner";
       inputs.pythoneda-shared-pythonlang-domain.follows =
         "pythoneda-shared-pythonlang-domain";
-      url = "github:pythoneda-runtime-infrastructure-def/eventstoredb/0.0.17";
+      url = "github:pythoneda-runtime-infrastructure-def/eventstoredb/0.0.18";
     };
     pythoneda-runtime-infrastructure-eventstoredb-infrastructure = {
       inputs.flake-utils.follows = "flake-utils";
@@ -41,7 +41,7 @@
       inputs.pythoneda-shared-pythonlang-domain.follows =
         "pythoneda-shared-pythonlang-domain";
       url =
-        "github:pythoneda-runtime-infrastructure-def/eventstoredb-infrastructure/0.0.17";
+        "github:pythoneda-runtime-infrastructure-def/eventstoredb-infrastructure/0.0.18";
     };
     pythoneda-shared-pythonlang-application = {
       inputs.flake-utils.follows = "flake-utils";
@@ -50,7 +50,7 @@
         "pythoneda-shared-pythonlang-banner";
       inputs.pythoneda-shared-pythonlang-domain.follows =
         "pythoneda-shared-pythonlang-domain";
-      url = "github:pythoneda-shared-pythonlang-def/application/0.0.78";
+      url = "github:pythoneda-shared-pythonlang-def/application/0.0.79";
     };
     pythoneda-shared-pythonlang-banner = {
       inputs.flake-utils.follows = "flake-utils";
@@ -62,7 +62,7 @@
       inputs.nixos.follows = "nixos";
       inputs.pythoneda-shared-pythonlang-banner.follows =
         "pythoneda-shared-pythonlang-banner";
-      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.77";
+      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.78";
     };
   };
   outputs = inputs:
