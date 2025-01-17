21c0bd581b168b5d13b7ce2a7d66e2b9f9182b17
rydnr <github@acm-sl.org>
2025-01-17 19:47:42 +0100

    Fixed unpackPhase

--- flake.nix
+++ flake.nix
@@ -178,16 +178,15 @@
             # pythonImportsCheck = [ pythonpackage ];
 
             unpackPhase = ''
-              command cp -r ${src} .
-              sourceRoot=$(command ls | command grep -v env-vars)
-              command chmod -R +w $sourceRoot
-              command cp ${pyprojectToml} $sourceRoot/pyproject.toml
-              command cp ${bannerTemplate} $sourceRoot/${banner_file}
-              command cp ${entrypointTemplate} $sourceRoot/entrypoint.sh
+              command cp -r ${src}/* .
+              command chmod -R +w .
+              command cp ${pyprojectToml} ./pyproject.toml
+              command cp ${bannerTemplate} ./${banner_file}
+              command cp ${entrypointTemplate} ./entrypoint.sh
             '';
 
             postPatch = ''
-              substituteInPlace /build/$sourceRoot/entrypoint.sh \
+              substituteInPlace ./entrypoint.sh \
                 --replace "@SOURCE@" "$out/bin/${entrypoint}.sh" \
                 --replace "@PYTHONPATH@" "$PYTHONPATH" \
                 --replace "@CUSTOM_CONTENT@" "" \
@@ -195,16 +194,14 @@
             '';
 
             postInstall = with python.pkgs; ''
-              command pushd /build/$sourceRoot
               for f in $(command find . -name '__init__.py'); do
                 if [[ ! -e $out/lib/python${pythonMajorMinorVersion}/site-packages/$f ]]; then
                   command cp $f $out/lib/python${pythonMajorMinorVersion}/site-packages/$f;
                 fi
               done
-              command popd
               command mkdir -p $out/dist $out/bin $out/deps/flakes
               cp dist/${wheelName} $out/dist
-              cp /build/$sourceRoot/entrypoint.sh $out/bin/${entrypoint}.sh
+              cp ./entrypoint.sh $out/bin/${entrypoint}.sh
               chmod +x $out/bin/${entrypoint}.sh
               for dep in ${pythoneda-runtime-infrastructure-eventstoredb} ${pythoneda-runtime-infrastructure-eventstoredb-infrastructure} ${pythoneda-shared-pythonlang-application} ${pythoneda-shared-pythonlang-banner} ${pythoneda-shared-pythonlang-domain}; do
                 command cp -r $dep/dist/* $out/deps || true
